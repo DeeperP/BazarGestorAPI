0c3d0394a2c21477127a9d22ddd5f9eea8d743fd
DeeperP <damianjcoria@gmail.com>
2025-06-12 13:55:23 -0300

    Primer commit: proyecto base Spring Boot creado

---
+++ src/main/java/com/example/BazarGestorAPI/BazarGestorApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BazarGestorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarGestorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarGestorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BazarGestorAPI/BazarGestorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BazarGestorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarGestorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
